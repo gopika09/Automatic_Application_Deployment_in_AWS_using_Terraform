b8e3619f74a499d459003247239df9d74028ee3f
GOPIKA G <70227399+gopika09@users.noreply.github.com>
2024-12-23 10:42:10 +0530

    Update main.tf

--- load_balancer/main.tf
+++ load_balancer/main.tf
@@ -1,55 +1,39 @@
-# Variable to store the EC2 security group ID
-variable "ec2_sg" {}
-
-# Variable to store the ARN of the Target Group
-variable "target_group_arn" {}
+# Variable to store the VPC ID
+variable "vpc_id" {}
 
 # Variable to store the EC2 instance ID
 variable "ec2_example_id" {}
 
-# Variable to store the list of subnet IDs
-variable "subnet_ids" {}
+# Variable to store availability zones
+variable "availability_zones" {}
 
-# Output the DNS name of the Load Balancer
-output "load_balancer_dns_name" {
-    value = aws_lb.load_balancer.dns_name  # Reference to the Load Balancer's DNS name
+# Output the ARN of the Target Group
+output "target_group_arn" {
+  value = aws_lb_target_group.example.arn  # Reference to the Target Group's ARN
 }
 
-# Output the Zone ID of the Load Balancer
-output "load_balancer_zone_id" {
-    value = aws_lb.load_balancer.zone_id    # Reference to the Load Balancer's Zone ID
-}
+# Create a Target Group for the Load Balancer
+resource "aws_lb_target_group" "example" {
+  name     = "target-group-for-load-balancer"  # Name of the Target Group
+  port     = 5000                               # Port on which the service is running
+  protocol = "HTTP"                             # Protocol used for communication
+  vpc_id   = var.vpc_id                         # Reference to the VPC ID
 
-# Create an Application Load Balancer
-resource "aws_lb" "load_balancer" {
-  name                          = "load-balancer"                   # Name of the Load Balancer
-  internal                      = false                               # Indicates this is an internet-facing Load Balancer
-  load_balancer_type            = "application"                     # Specifies the type of Load Balancer
-  security_groups               = [var.ec2_sg]                      # Reference to the security group for the Load Balancer
-  enable_deletion_protection    = false                               # Allows deletion of the Load Balancer
-  subnets                       = var.subnet_ids             # List of subnet IDs for Load Balancer placement
-
-  tags = {
-    Name = "load_balancer_for_ec2_instance"  # Tags for identifying the Load Balancer
+  # Health check configuration for the Target Group
+  health_check {
+    path                = "/"                    # Path for the health check
+    port                = 5000                   # Port for the health check
+    interval            = 30                      # Time (in seconds) between health checks
+    timeout             = 5                       # Time (in seconds) to wait for a response
+    healthy_threshold  = 2                       # Number of successful checks before marking healthy
+    unhealthy_threshold = 2                      # Number of failed checks before marking unhealthy
+    matcher             = "200"                  # HTTP status code for a successful response
   }
 }
 
 # Register the EC2 Instance to the Target Group
-resource "aws_lb_target_group_attachment" "load_balancer_target_group" {
-  target_group_arn = var.target_group_arn  # Reference to the Target Group's ARN
-  target_id        = var.ec2_example_id      # Reference to the EC2 instance ID
-  port             = 5000                    # Port for the EC2 instance in the Target Group
-}
-
-# Create a Listener for the Load Balancer
-resource "aws_lb_listener" "load_balancer_listener_http" {
-  load_balancer_arn = aws_lb.load_balancer.arn  # Reference to the Load Balancer's ARN
-  port              = 80                          # Port for incoming traffic
-  protocol          = "HTTP"                      # Protocol for the listener
-
-  # Default action to forward traffic to the target group
-  default_action {
-    type             = "forward"                  # Action type
-    target_group_arn = var.target_group_arn      # Reference to the Target Group's ARN
-  }
+resource "aws_lb_target_group_attachment" "example" {
+  target_group_arn = aws_lb_target_group.example.arn  # Reference to the Target Group's ARN
+  target_id        = var.ec2_example_id                 # Reference to the EC2 instance ID
+  port             = 5000                               # Port for the EC2 instance in the Target Group
 }
