9409fa8044bfd2cddaf1743203211cd06d76de85
GOPIKA G <70227399+gopika09@users.noreply.github.com>
2024-12-23 10:46:34 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,7 +10,28 @@ variable "public_cidr_block" {
   description = "Public Subnet CIDR value"  # Description of the variable's purpose
 }
 
-/* 
-# Define the CIDR blocks for private subnets (currently commented out)
+# Define the CIDR blocks for private subnets
 variable "private_cidr_block" {
-  type        = lis
+  type        = list(string)  # Type is a list of strings
+  description = "Private Subnet CIDR value"  # Description of the variable's purpose
+}
+
+variable "availability_zones" {
+  type        = list(string)  # Type is a list of strings
+  description = "Availability zones for VPC"  # Description of the variable's purpose
+}
+
+variable "ami" {
+  type = string
+  description = "ami of EC2"
+}
+
+variable "instance_type" { 
+  type = string
+  description = "Instance type of EC2"
+}
+
+variable "public_key" {
+  type = string
+  description = "public key for SSH access"
+}
